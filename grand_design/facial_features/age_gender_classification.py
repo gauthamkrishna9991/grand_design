bd4ad4ed7bcdbe291edd256288c684b7cc6c0886
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-14 23:24:48 +0530

    agegender update

--- grand_design/facial_features/age_gender_classification.py
+++ grand_design/facial_features/age_gender_classification.py
@@ -5,7 +5,8 @@ import numpy as np
 import argparse
 from contextlib import contextmanager
 from wide_resnet import WideResNet
-from keras.utils.data_utils import get_file
+
+
 
 pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
 modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
@@ -78,13 +79,10 @@ def main():
 #    args = get_args()
     depth = 16
     k = 8
-    weight_file = 'path_to_weights'
+    weight_file = 'agegender.hdf5'
     margin = 0.4
-    image_dir = 'use this for setting image directory instead of webcam'
+    img = cv2.imread('img.jpg')
 
-    if not weight_file:
-        weight_file = get_file("weights.28-3.73.hdf5", pretrained_model, cache_subdir="pretrained_models",
-                               file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
 
     # for face detection
     detector = dlib.get_frontal_face_detector()
@@ -94,44 +92,42 @@ def main():
     model = WideResNet(img_size, depth=depth, k=k)()
     model.load_weights(weight_file)
 
-    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+    
 
-    for img in image_generator:
-        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_h, img_w, _ = np.shape(input_img)
+   
+    input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_h, img_w, _ = np.shape(input_img)
 
-        # detect faces using dlib detector
-        detected = detector(input_img, 1)
-        faces = np.empty((len(detected), img_size, img_size, 3))
+    # detect faces using dlib detector
+    detected = detector(input_img, 1)
+    faces = np.empty((len(detected), img_size, img_size, 3))
 
-        if len(detected) > 0:
-            for i, d in enumerate(detected):
-                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                xw1 = max(int(x1 - margin * w), 0)
-                yw1 = max(int(y1 - margin * h), 0)
-                xw2 = min(int(x2 + margin * w), img_w - 1)
-                yw2 = min(int(y2 + margin * h), img_h - 1)
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+    if len(detected) > 0:
+        for i, d in enumerate(detected):
+            x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+            xw1 = max(int(x1 - margin * w), 0)
+            yw1 = max(int(y1 - margin * h), 0)
+            xw2 = min(int(x2 + margin * w), img_w - 1)
+            yw2 = min(int(y2 + margin * h), img_h - 1)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+            faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
 
-            # predict ages and genders of the detected faces
-            results = model.predict(faces)
-            predicted_genders = results[0]
-            ages = np.arange(0, 101).reshape(101, 1)
-            predicted_ages = results[1].dot(ages).flatten()
+        # predict ages and genders of the detected faces
+        results = model.predict(faces)
+        predicted_genders = results[0]
+        ages = np.arange(0, 101).reshape(101, 1)
+        predicted_ages = results[1].dot(ages).flatten()
 
-            # draw results
-            for i, d in enumerate(detected):
-                label = "{}, {}".format(int(predicted_ages[i]),
-                                        "M" if predicted_genders[i][0] < 0.5 else "F")
-                draw_label(img, (d.left(), d.top()), label)
+        # draw results
+        for i, d in enumerate(detected):
+            label = "{}, {}".format(int(predicted_ages[i]),
+                                    "M" if predicted_genders[i][0] < 0.5 else "F")
+            draw_label(img, (d.left(), d.top()), label)
 
-        cv2.imshow("result", img)
-        key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
+        cv2.imwrite("result.jpg", img)
+        print('done')
 
-        if key == 27:  # ESC
-            break
 
 
 if __name__ == '__main__':
