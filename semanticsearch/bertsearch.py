9bc18d377d5b46d8c39f331f1f74ef1ed7ab086d
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-11-21 20:44:26 +0530

    small dig

--- semanticsearch/bertsearch.py
+++ semanticsearch/bertsearch.py
@@ -9,27 +9,12 @@ Created on Tue Nov 19 22:04:34 2019
 #from bert_serving.client import BertClient
 #bc = BertClient()
 #bc.encode(['First do it', 'then do it right', 'then do it better'])
-from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
+from clio_lite import clio_keywords
+query = "BERT"
+filters = [{"range":{"year_of_article":{"gte":"2018"}}}]
+keywords = clio_keywords(url=url, index=index, query=query, 
+                         fields=['textBody_abstract_article','title_of_article'],
+                         filters=filters)
 
-
-LANGUAGE = "english"
-SENTENCES_COUNT = 10
-
-
-if __name__ == "__main__":
-    url = "https://en.wikipedia.org/wiki/Automatic_summarization"
-    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-    # or for plain text files
-    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
-    stemmer = Stemmer(LANGUAGE)
-
-    summarizer = Summarizer(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
-
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+for kw in keywords:
+    print(kw) 
