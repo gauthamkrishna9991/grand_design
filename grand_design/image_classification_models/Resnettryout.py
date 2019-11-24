16c61f762f6b2be982fba3fe50739732bfda3260
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-11-24 22:09:39 +0530

    resnettryout

---
+++ grand_design/image_classification_models/Resnettryout.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 20:51:05 2019
+
+@author: abhijithneilabraham
+"""
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+
+img_path = 'dog.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+print('Predicted:', decode_predictions(preds, top=3)[0])
+d=decode_predictions(preds, top=3)[0]
+for i in d:
+    print(i[1])
